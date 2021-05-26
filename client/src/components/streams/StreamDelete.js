9949e1ffe1ae7534b861d43216d3f266adcd7a74
Alfonso Jim <alfonsojim1897@gmail.com>
2021-05-26 18:20:15 +0200

    added refactor for modal reusability

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,10 +2,21 @@ import React from "react";
 import Modal from "../Modal";
 
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </div>
+  );
+
   return (
     <div>
       <h3>Delete a Stream</h3>
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delte this stream?"
+        actions={actions}
+      />
     </div>
   );
 };
